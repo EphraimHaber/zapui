be719c6c279324a925af56f5f409da00fcce8b7e
Bikas KC <bikas@87-224-45-153.spitfireuk.net>
2025-02-21 17:36:46 +0000

    feat(test) - Button: formatting updates on testing file

--- projects/zap/core/button/button.component.spec.ts
+++ projects/zap/core/button/button.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { ZapButton } from './button.component';
-import { ZapIconDirective } from '../directives/icon.directive';
 
 describe('ZapButton', () => {
     let component: ZapButton;
