1e60cee45cc3a8417ba652994a09ebdb37de79ad
Bikas KC <bikas@87-224-45-153.spitfireuk.net>
2025-03-14 15:26:14 +0000

    fix/theme: global styles is now working; tooltip component interface fixed

--- projects/zap/src/lib/interfaces/tooltip.interface.ts
+++ projects/zap/src/lib/interfaces/tooltip.interface.ts
@@ -6,12 +6,12 @@ export interface TooltipConfig {
     colors?: Record<
       string,
       {
-        handlerBgColor?: HexCode
-        handlerTextColor?: HexCode
-        handlerBorderColor?: HexCode
-        handlerBgHoverColor?: HexCode
-        handlerTextHoverColor?: HexCode
-        handlerBorderHoverColor?: HexCode
+        bgColor?: HexCode
+        textColor?: HexCode
+        borderColor?: HexCode
+        bgHoverColor?: HexCode
+        textHoverColor?: HexCode
+        borderHoverColor?: HexCode
         contentBgColor?: HexCode
         contentTextColor?: HexCode
         contentBorderColor?: HexCode
@@ -27,10 +27,5 @@ export interface TooltipConfig {
     contentFontWeight?: string
     contentLineHeight?: string
     contentLetterSpacing?: string
-    handlerBorderRadius?: string
-    handlerFontSize?: string
-    handlerFontWeight?: string
-    handlerLineHeight?: string
-    handlerLetterSpacing?: string
   }
 }
